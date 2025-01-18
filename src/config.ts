70f2394c0fefa2ae51f0a70c271c584e213c73f2
Henry Mao <1828968+calclavia@users.noreply.github.com>
2025-01-18 21:54:17 +0800

    Make config optional

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
-export function createSmitheryUrl(baseUrl: string, config: object) {
+export function createSmitheryUrl(baseUrl: string, config?: object) {
 	const url = new URL(baseUrl)
-	const param =
-		typeof window !== "undefined"
-			? btoa(JSON.stringify(config))
-			: Buffer.from(JSON.stringify(config)).toString("base64")
-	url.searchParams.set("config", param)
+	if (config) {
+		const param =
+			typeof window !== "undefined"
+				? btoa(JSON.stringify(config))
+				: Buffer.from(JSON.stringify(config)).toString("base64")
+		url.searchParams.set("config", param)
+	}
 	return url
 }
