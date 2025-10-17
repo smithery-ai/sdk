49d05fb53d3b29f6f5338e15565a535f198571b2
arjunkmrm <arjunkmrm@gmail.com>
2025-10-17 20:45:22 +0800

    style

--- typescript/sdk/src/react/ErrorBoundary.ts
+++ typescript/sdk/src/react/ErrorBoundary.ts
@@ -10,8 +10,10 @@ interface ErrorBoundaryState {
 	error: Error | null
 }
 
-export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
-
+export class ErrorBoundary extends Component<
+	ErrorBoundaryProps,
+	ErrorBoundaryState
+> {
 	constructor(props: ErrorBoundaryProps) {
 		super(props)
 		this.state = { hasError: false, error: null }
