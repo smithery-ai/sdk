55bb1e084c8a46b90a5ff2fb350724023eaf82ca
Henry Mao <1828968+calclavia@users.noreply.github.com>
2025-11-25 19:22:43 +0800

    Orphan file fix

--- typescript/registry/src/lib/config.ts
+++ typescript/registry/src/lib/config.ts
@@ -59,7 +59,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.5.3",
-  genVersion: "2.760.2",
-  userAgent: "speakeasy-sdk/typescript 0.5.3 2.760.2 1.0.0 @smithery/registry",
+  sdkVersion: "0.5.4",
+  genVersion: "2.743.2",
+  userAgent: "speakeasy-sdk/typescript 0.5.4 2.743.2 1.0.0 @smithery/registry",
 } as const;
